0d5ccf59b30fc309f35875dab3550414875b1c13
Manas Sharma <manasdotsharma@gmail.com>
2024-07-12 02:12:07 +0530

    add navbar

--- lib/components/NavBar.jsx
+++ lib/components/NavBar.jsx
@@ -8,12 +8,9 @@ import {
   Disclosure,
   DisclosureButton,
   DisclosurePanel,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems,
 } from "@headlessui/react";
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Logo } from "./Logo";
 
 // const items = [
 //   { name: 'Dashboard', href: '#', current: true },
@@ -22,14 +19,50 @@ import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 //   { name: 'Calendar', href: '#', current: false },
 // ]
 
-export function NavBar({ items }) {
+export function NavBar({ items = [], rootClassNames = "" }) {
   return (
-    <Disclosure as="nav" className="bg-gray-800">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            {/* Mobile menu button*/}
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+    <Disclosure as="nav" className={twMerge("bg-white shadow", rootClassNames)}>
+      <div className="mx-auto max-w-7xl px-4 xl:px-6 lg:px-8">
+        <div className="flex h-12 justify-between">
+          <div className="flex">
+            <div className="flex flex-shrink-0 items-center">
+              <Logo classNames="h-6" />
+            </div>
+            <div className="hidden xl:ml-6 xl:flex xl:space-x-8">
+              {/* Current: "border-blue-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
+              {items.map(
+                (item) =>
+                  item.render || (
+                    <a
+                      key={item.title}
+                      href={item.href}
+                      onClick={(e) => {
+                        item?.onClick ? item.onClick(e) : null;
+                      }}
+                      className={twMerge(
+                        "inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm text-gray-500 hover:border-gray-300 hover:text-gray-700",
+                        item.current ? "border-blue-500 text-gray-900" : ""
+                      )}
+                      aria-current={item.current ? "page" : undefined}
+                    >
+                      {item.title}
+                    </a>
+                  )
+              )}
+            </div>
+          </div>
+          <div className="hidden xl:ml-6 xl:flex xl:items-center">
+            <button
+              type="button"
+              className="relative rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+            >
+              <span className="absolute -inset-1.5" />
+              <span className="sr-only">View notifications</span>
+            </button>
+          </div>
+          <div className="-mr-2 flex items-center xl:hidden">
+            {/* Mobile menu button */}
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500">
               <span className="absolute -inset-0.5" />
               <span className="sr-only">Open main menu</span>
               <Bars3Icon
@@ -42,109 +75,33 @@ export function NavBar({ items }) {
               />
             </DisclosureButton>
           </div>
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex flex-shrink-0 items-center">
-              <img
-                alt="Your Company"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                className="h-8 w-auto"
-              />
-            </div>
-            <div className="hidden sm:ml-6 sm:block">
-              <div className="flex space-x-4">
-                {items.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    aria-current={item.current ? "page" : undefined}
-                    className={twMerge(
-                      item.current
-                        ? "bg-gray-900 text-white"
-                        : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                      "rounded-md px-3 py-2 text-sm font-medium"
-                    )}
-                  >
-                    {item.name}
-                  </a>
-                ))}
-              </div>
-            </div>
-          </div>
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <button
-              type="button"
-              className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-            >
-              <span className="absolute -inset-1.5" />
-              <span className="sr-only">View notifications</span>
-              <BellIcon aria-hidden="true" className="h-6 w-6" />
-            </button>
-
-            {/* Profile dropdown */}
-            <Menu as="div" className="relative ml-3">
-              <div>
-                <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">Open user menu</span>
-                  <img
-                    alt=""
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    className="h-8 w-8 rounded-full"
-                  />
-                </MenuButton>
-              </div>
-              <MenuItems
-                transition
-                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-              >
-                <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
-                  >
-                    Your Profile
-                  </a>
-                </MenuItem>
-                <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
-                  >
-                    Settings
-                  </a>
-                </MenuItem>
-                <MenuItem>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100"
-                  >
-                    Sign out
-                  </a>
-                </MenuItem>
-              </MenuItems>
-            </Menu>
-          </div>
         </div>
       </div>
 
-      <DisclosurePanel className="sm:hidden">
-        <div className="space-y-1 px-2 pb-3 pt-2">
-          {items.map((item) => (
-            <DisclosureButton
-              key={item.name}
-              as="a"
-              href={item.href}
-              aria-current={item.current ? "page" : undefined}
-              className={twMerge(
-                item.current
-                  ? "bg-gray-900 text-white"
-                  : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                "block rounded-md px-3 py-2 text-base font-medium"
-              )}
-            >
-              {item.name}
-            </DisclosureButton>
-          ))}
+      <DisclosurePanel className="xl:hidden max-w-7xl px-4 xl:px-6">
+        <div className="space-y-1 pb-3 pt-2">
+          {items.map(
+            (item) =>
+              item.render || (
+                <DisclosureButton
+                  key={item.title}
+                  as="a"
+                  href={item.href}
+                  onClick={(e) => {
+                    item?.onClick ? item.onClick(e) : null;
+                  }}
+                  className={twMerge(
+                    "block border-l-4 border-transparent py-2 pl-3 pr-4 text-base text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700",
+                    item.current
+                      ? "border-blue-500 bg-blue-50 text-blue-700"
+                      : ""
+                  )}
+                  aria-current={item.current ? "page" : undefined}
+                >
+                  {item.title}
+                </DisclosureButton>
+              )
+          )}
         </div>
       </DisclosurePanel>
     </Disclosure>
