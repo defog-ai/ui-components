67b57bf9ba75142d5a63cec8de361730bc70a363
Manas Sharma <manasdotsharma@gmail.com>
2024-07-12 16:34:50 +0530

    fix drop files

--- lib/components/DropFiles.jsx
+++ lib/components/DropFiles.jsx
@@ -5,16 +5,24 @@ import { twMerge } from "tailwind-merge";
 export function DropFiles({
   label = "Drop files here",
   onDrop = (...args) => {},
+  onFileSelect = (...args) => {},
   onDragOver = (...args) => {},
+  onDragEnter = (...args) => {},
   rootClassNames = "",
   iconClassNames = "",
+  children = null,
+  icon = null,
 }) {
   return (
     <div
-      className={twMerge("text-gray-600 text-center", rootClassNames)}
+      className={twMerge("relative text-gray-600 text-center", rootClassNames)}
       onDrop={(e) => onDrop(e)}
+      onDragEnter={(e) => {
+        e.preventDefault();
+        onDragEnter(e);
+      }}
       onDragOver={(e) => {
-        // Preent default behavior (Prevent file from being opened)
+        // Prevent default behavior (Prevent file from being opened)
         e.preventDefault();
         onDragOver(e);
       }}
@@ -24,9 +32,27 @@ export function DropFiles({
           {label}
         </label>
       )}
-      <ArrowDownTrayIcon
-        className={twMerge("m-auto h-6 w-6", iconClassNames)}
-      />
+      {children}
+      <div className="flex flex-col items-center justify-center">
+        {icon || (
+          <ArrowDownTrayIcon className={twMerge("h-6 w-6", iconClassNames)} />
+        )}
+        <div className="mt-2 relative group">
+          <p className="text-xs text-gray-400 group-hover:underline z-[2] relative pointer-events-none">
+            Select from your computer
+          </p>
+          <input
+            aria-label=""
+            accept="text/csv"
+            className="w-full h-full z-[1] opacity-0 absolute left-0 top-0"
+            type="file"
+            onInput={(e) => {
+              e.preventDefault();
+              onFileSelect(e);
+            }}
+          ></input>
+        </div>
+      </div>
     </div>
   );
 }
